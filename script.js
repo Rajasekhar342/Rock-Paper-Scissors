7deb7cf377a3fa67db2207f215e076564bc43b35
rajasekhar_bevara <rajasekharbevara341@gmail.com>
2022-04-11 17:45:35 +0530

    added function playRound

--- script.js
+++ script.js
@@ -5,12 +5,25 @@ const random = Math.floor(Math.random() * choose.length);
 console.log(choose[random]);
 return (choose[random]);
 }
-const playerSelection = prompt("enter rock,paper or scissors:");
-playerSelection.toLowerCase();
+let playerSelection = prompt("Enter rock,paper or scissors:");
+playerSelection=playerSelection.toLowerCase();
 const computerSelection = computerPlay();
 console.log(computerSelection)
 function playRound(playerSelection, computerSelection) {
-  
-}
+  if (playerSelection===computerSelection){
+    return "it's tie"
+  }
+  if ((computerSelection==="rock"&&playerSelection==="paper")
+  ||(computerSelection==="paper"&&playerSelection==="scissors")
+  ||(computerSelection==="scissors"&&playerSelection==="rock")){
+    return "you won"
 
+  }
+  if ((playerSelection==="rock"&&computerSelection==="paper")
+  ||(playerSelection==="paper"&&computerSelection==="scissors")
+  ||(playerSelection==="scissors"&&computerSelection==="rock")){
+    return "you lose"
+  }  
+}
+console.log(playRound(playerSelection,computerSelection))
 
